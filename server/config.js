96f31caac1fa9fe3327702d48cf75c96b814deb8
Romanov7788 <romanov_sash@ukr.net>
2022-10-05 23:21:20 +0300

    еее

--- server/config.js
+++ server/config.js
@@ -4,4 +4,5 @@ module.exports = {
     "mongodb+srv://romanov:romanov@cluster0.d87zx.mongodb.net/dbForApi?retryWrites=true&w=majority",
   JWT_ACCESS_KEY: "secret",
   JWT_REFRESH_KEY: "secret key",
+  
 };
